fed4c30a619a1567e8ce0ba6cf66047e78ba6462
Sandeep Machiraju <30957455+machirajusaisandeep@users.noreply.github.com>
2025-02-05 20:19:02 +0530

    Add product-related components, model, and service with Bootstrap integration

---
+++ src/app/components/filter-sidebar/filter-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-sidebar',
+  imports: [],
+  templateUrl: './filter-sidebar.component.html',
+  styleUrl: './filter-sidebar.component.css'
+})
+export class FilterSidebarComponent {
+
+}
