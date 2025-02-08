0b1655a14c709d3a6324a13c743018eb2f0f3b26
Sandeep Machiraju <30957455+machirajusaisandeep@users.noreply.github.com>
2025-02-08 12:06:17 +0530

    Refine UI components with updated styles and responsive design

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [NgbRatingModule],
+  imports: [CommonModule, NgbRatingModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
@@ -27,5 +28,9 @@ export class ProductCardComponent {
     } else {
       this.productService.addToWishlist(this.product);
     }
+  }
+
+  getRating(): number {
+    return Number(this.product.rating);
   }
 }
